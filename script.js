29219efdf8a96cc8952c7aadaf0c47744f8e5020
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-24 01:50:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	if(s==="" || s.trin()===""){
+		return ""
+	}
+	s=s.trim();
+	let a = s.split(" ");
+	return a[0]
 }
 
 // Do not change the code below
